ec62ce5c2a63af823f96889d86a64021534ad6d5
GET Academy <terje@getacademy.no>
2022-06-28 12:51:22 +0200

    Initial commit

---
+++ CharactersRefactored/Character.cs
@@ -0,0 +1,25 @@
+﻿namespace CharactersRefactored
+{
+    internal class Character
+    {
+        private readonly string _characters;
+        int _index;
+
+        public char Char => _characters[_index];
+        public static Character Vowel => new Character("AEIOUYÆØÅ");
+        public static Character Consonant => new Character("BCDFGHJKLMNPQRSTVWXZ");
+
+        public Character(string characters)
+        {
+            _characters = characters;
+        }
+
+        public bool Next()
+        {
+            _index++;
+            if (_index < _characters.Length) return false;
+            _index = 0;
+            return true;
+        }
+    }
+}
---
+++ CharactersRefactored/Program.cs
@@ -0,0 +1,44 @@
+﻿using CharactersRefactored;
+
+var pattern = args[0].ToUpper();
+var characters = pattern.Select(c => c == 'V' ? Character.Vowel : Character.Consonant
+    ).ToArray();
+while (true)
+{
+    Console.WriteLine(characters);
+    var index = characters.Length - 1;
+    var continueToNextCharacter = true;
+    while (continueToNextCharacter)
+    {
+        if (pattern[index] == 'V')
+        {
+            if (characters[index] == 'Å')
+            {
+                if (index == 0) return;
+                characters[index] = 'A';
+            }
+            else
+            {
+                var i = vowels.IndexOf(characters[index]);
+                characters[index] = vowels[i + 1];
+                continueToNextCharacter = false;
+            }
+        }
+        else if (pattern[index] == 'K')
+        {
+            if (characters[index] == 'Z')
+            {
+                if (index == 0) return;
+                characters[index] = 'B';
+            }
+            else
+            {
+                var i = consonants.IndexOf(characters[index]);
+                characters[index] = consonants[i + 1];
+                continueToNextCharacter = false;
+            }
+        }
+
+        index--;
+    }
+}
